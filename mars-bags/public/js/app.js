2245b2b74f41462584d24cbc09764e62511ce24e
ToasterTom <tate.thomas5595@gmail.com>
2016-11-03 23:40:24 -0600

    Hopefully the files are the way I want them

---
+++ mars-bags/public/js/app.js
@@ -0,0 +1,46 @@
+angular.module('mars-bags', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home',{
+                url:'/',
+                templateUrl: "public/views/homeTpl.html"
+            })
+            .state('shop',{
+              url:'/shop',
+
+              templateUrl: "public/views/shopTpl.html"
+            })
+            .state('about',{
+              url:'/about',
+
+              templateUrl: "public/views/aboutTpl.html"
+            })
+            .state('charity-water',{
+              url:'/charity-water',
+
+              templateUrl: "public/views/charity-waterTpl.html"
+            })
+            .state('news-events',{
+              url:'/news-events',
+
+              templateUrl: "public/views/news-eventsTpl.html"
+            })
+            .state('checkout',{
+              url:'/checkout',
+
+              templateUrl: "public/views/checkoutTpl.html"
+            })
+            .state('terms-conditions',{
+              url:'/terms-conditions',
+
+              templateUrl: "public/views/terms-conditionsTpl.html"
+            })
+            .state('contact-us',{
+              url:'/contact-us',
+
+              templateUrl: "public/views/contact-usTpl.html"
+            })
+
+          $urlRouterProvider
+            .otherwise('/');
+    });
